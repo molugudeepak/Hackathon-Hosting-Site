8a5c84850d3d548c4295b3a0c63e88281a78b894
Deepak M <Deepak.kanna@gmail.com>
2021-08-27 12:09:11 -0400

    Adding login and registration components

---
+++ registration.php
@@ -0,0 +1,31 @@
+ <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Registration</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+  
+<link rel="stylesheet" href="style1.css" />
+<header>
+<div class="container">
+ 
+        
+</div>
+  
+</header>
+<h1 style="font-size:45px;text-align:center;color:#FF0000">Hackathon Hosting Site </h1>
+    <form class="form" action="" method="post">
+        <h1 class="login-title">Registration</h1>
+        <input type="text" class="login-input" name="name" placeholder="Name" required />
+        <input type="text" class="login-input" name="mobile" placeholder="Mobile No" required />
+        <input type="text" class="login-input" name="email" placeholder="Email Adress">
+        <input type="text" class="login-input" name="username" placeholder="Username" required />
+        <input type="password" class="login-input" name="password" placeholder="Password">
+        <input type="submit" name="submit" value="Register" class="login-button">
+        <p class="link"><a href="index.php">Click to Login</a></p>
+    </form>
+    </div>
+</body>
+</html>
