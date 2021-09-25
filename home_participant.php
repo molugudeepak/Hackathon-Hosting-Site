d4e7a34d65f79ca444548e106f4b241a934ec9a2
Deepak M <Deepak.kanna@gmail.com>
2021-09-24 21:36:59 -0400

    updated participant home page

--- home_participant.php
+++ home_participant.php
@@ -2,10 +2,46 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>Hackathon Hosting Site</title>
+    <title>Registration</title>
     <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-<p>This is Homepage for Participant</p>
+  
+<link rel="stylesheet" href="style1.css" />
+<header>
+<div class="container">
+</div>
+</header>
+<h1 style="font-size:45px;text-align:center;color:#FF0000">Hackathon Hosting Site </h1>
+<div class="form">
+<h1 class="login-title">Register for an event </h1>
+<?php
+    require('db.php');
+    // When form submitted, insert values into the database.
+    $result = mysqli_query($con,"SELECT * FROM events");
+   
+    echo "<table border='1'> 
+    <tr>
+    <th>Event Name</th>
+    <th>Venue</th>
+    <th>Start Date</th>
+    <th>End Date</th>
+    </tr>";
+    while($row = mysqli_fetch_array($result))
+    {
+    echo "<tr>";
+    echo "<td>" . $row['event_name'] . "</td>";
+    echo "<td>" . $row['event_location'] . "</td>";
+    echo "<td>" . $row['event_startdate'] . "</td>";
+    echo "<td>" . $row['event_enddate']. "</td>";
+    
+    
+    echo "</tr>";
+    }
+    echo "</table>";
+
+
+?>
+</div>
 </body>
 </html>
